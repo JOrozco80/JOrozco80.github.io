5b58327a4969ecfc8a35e8798358b39e89b90058
Juan H. Orozco <89162304+JOrozco80@users.noreply.github.com>
2021-10-30 12:33:56 -0400

    Delete JuanWebpageJavaScript.js

--- JuanWebpageJavaScript.js
+++
@@ -1,5 +0,0 @@
-/*Here is my first attempt at adding Javascript element to my WebSite. The landing page will display the alert. Once the user clicks ok the second prompt will asj the user to enter their email address.
-The email address does not get deposited anywhere, so the user can enter anything. and press ok to continue viewing the page.*/
-
-alert("This was created in JavaScript")
-prompt("Please enter an email address where you will receive updates.")
