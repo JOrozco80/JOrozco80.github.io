06a9e0dc984949c26e7bbee19570b7beaed92efb
Juan H. Orozco <89162304+JOrozco80@users.noreply.github.com>
2021-10-30 12:42:15 -0400

    Create juanwebpageJS.js

---
+++ juanwebpageJS.js
@@ -0,0 +1,4 @@
+// JavaScript the displays an alert window with the message "This is my attept at JS(JavaScript)//
+
+alert("This was created in JavaScript")
+prompt("Please enter an email address where you will receive updates.")
