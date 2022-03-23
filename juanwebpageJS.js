c1f070404e14f32fded4b6c9061bd01ad642120b
Juan H. Orozco <89162304+JOrozco80@users.noreply.github.com>
2022-03-23 15:34:46 -0400

    Update juanwebpageJS.js

--- juanwebpageJS.js
+++ juanwebpageJS.js
@@ -1,4 +1,9 @@
 // JavaScript the displays an alert window with the message "This is my attept at JS(JavaScript)//
 
 alert("This was created in JavaScript")
-prompt("Please enter an email address where you will receive updates.")
+
+window.confirm()
+if (confirm("Press a button!")) {
+  txt = "Ypu pressed OK!";
+} else {
+  txt = "You pressed cancel!";}
